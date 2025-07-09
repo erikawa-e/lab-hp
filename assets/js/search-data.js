3a1041253546f203362457881a9b7ca1681993b3
erikawa-e <79493589+erikawa-e@users.noreply.github.com>
2025-07-09 10:35:20 +0000

    Deploying to gh-pages from @ erikawa-e/lab-hp@627c4b5f3a8217cf340ea2f8fa1d560e800c949b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -349,13 +349,13 @@ ninja.data = [{
         },
       },{id: "post-a-post-with-redirect",
         
-          title: "a post with redirect",
+          title: 'a post with redirect <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
         description: "you can also redirect to assets like pdf",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/lab-hp/assets/pdf/en-us/example_pdf.pdf";
+            window.open("https://riken-share.box.com/s/xv9oj2sux7ms2zggatrbcfh1dd9jft0r", "_blank");
           
         },
       },{id: "post-a-post-with-diagrams",
