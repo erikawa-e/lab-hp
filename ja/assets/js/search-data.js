8fc34a32dce6f4c75d87e954e6bc0a9ed13384b8
erikawa-e <79493589+erikawa-e@users.noreply.github.com>
2026-04-21 15:45:08 +0000

    Deploying to gh-pages from @ erikawa-e/lab-hp@03a9f1a552768d5165f27843c74578693601b25e 🚀

--- ja/assets/js/search-data.js
+++ ja/assets/js/search-data.js
@@ -16,84 +16,84 @@ const ninja = document.querySelector('ninja-keys');
 ninja.data = [{
     id: "nav-about",
     title: "about",
-    section: "",
+    section: "Navigation menu",
     handler: () => {
       window.location.href = "/lab-hp/ja/";
     },
   },{id: "nav-ブログ",
           title: "ブログ",
           description: "研究室のニュースとアップデート",
-          section: "",
+          section: "Navigation menu",
           handler: () => {
             window.location.href = "/lab-hp/ja/blog/";
           },
         },{id: "nav-発表論文",
           title: "発表論文",
           description: "発表論文一覧（新しい順）。jekyll-scholarにより自動生成。",
-          section: "",
+          section: "Navigation menu",
           handler: () => {
             window.location.href = "/lab-hp/ja/publications/";
           },
         },{id: "nav-メンバー",
           title: "メンバー",
           description: "FEBQIチームのメンバー",
-          section: "",
+          section: "Navigation menu",
           handler: () => {
             window.location.href = "/lab-hp/ja/people/";
           },
         },{id: "nav-プロジェクト",
           title: "プロジェクト",
           description: "Uma crescente coleção de seus projetos interessantes.",
-          section: "",
+          section: "Navigation menu",
           handler: () => {
             window.location.href = "/lab-hp/ja/projects/";
           },
         },{id: "nav-リポジトリ",
           title: "リポジトリ",
           description: "Edite o `_data/repositories.yml` e mude as listas `github_users` e `github_repos` para incluir seu próprio perfil do GitHub e repositórios.",
-          section: "",
+          section: "Navigation menu",
           handler: () => {
             window.location.href = "/lab-hp/ja/repositories/";
           },
         },{id: "nav-cv",
           title: "cv",
           description: "Esta é uma descrição da página. Você pode modificá-la em &#39;_pages/cv.md&#39;. Também pode alterar ou remover o botão no topo de download de pdf.",
-          section: "",
+          section: "Navigation menu",
           handler: () => {
             window.location.href = "/lab-hp/ja/cv/";
           },
         },{id: "nav-教育",
           title: "教育",
           description: "Materiais de cursos que você ministrou. Substitua esse texto com sua descrição.",
-          section: "",
+          section: "Navigation menu",
           handler: () => {
             window.location.href = "/lab-hp/ja/teaching/";
           },
         },{id: "nav-アウトリーチ",
           title: "アウトリーチ",
           description: "解説記事・一般向け講演など",
-          section: "",
+          section: "Navigation menu",
           handler: () => {
             window.location.href = "/lab-hp/ja/outreach/";
           },
         },{id: "dropdown-publicações",
               title: "publicações",
               description: "",
-              section: "",
+              section: "Dropdown",
               handler: () => {
                 window.location.href = "/lab-hp/ja/publications/";
               },
             },{id: "dropdown-projetos",
               title: "projetos",
               description: "",
-              section: "",
+              section: "Dropdown",
               handler: () => {
                 window.location.href = "/lab-hp/ja/projects/";
               },
             },{id: "dropdown-blog",
               title: "blog",
               description: "",
-              section: "",
+              section: "Dropdown",
               handler: () => {
                 window.location.href = "/lab-hp/ja/blog/";
               },
@@ -102,7 +102,7 @@ ninja.data = [{
           title: "",
         
         description: "",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2026/2021-11-04-yappari-butsuri/";
@@ -113,7 +113,7 @@ ninja.data = [{
           title: "嬉しいニュース",
         
         description: "Hochan Chungが学士論文優秀賞を受賞",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2026/hochan-award/";
@@ -124,7 +124,7 @@ ninja.data = [{
           title: "お誕生日おめでとう",
         
         description: "研究室のお誕生日のお祝い",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2026/birthdays/";
@@ -135,7 +135,7 @@ ninja.data = [{
           title: "フランスの同窓会誌に量子コンピューティングの解説記事が掲載",
         
         description: "山田、Giovansili、川上による解説記事がフランスの同窓会誌に掲載",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2026/french-review-article/";
@@ -146,7 +146,7 @@ ninja.data = [{
           title: "嬉しいニュース",
         
         description: "ポスター賞受賞とPRL論文がRIKEN Research Newsに掲載",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2026/poster-award-prl-news/";
@@ -157,7 +157,7 @@ ninja.data = [{
           title: "国際共同研究プロジェクト採択",
         
         description: "JST ASPIREプログラムに採択",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2025/jst-aspire/";
@@ -168,7 +168,7 @@ ninja.data = [{
           title: "Tetsuの送別会",
         
         description: "山田哲義の送別会",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2025/tetsu-farewell/";
@@ -179,7 +179,7 @@ ninja.data = [{
           title: "嬉しいニュース",
         
         description: "PRL論文がEditors&#39; Suggestionに選出、LT30でポスター賞受賞",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2025/prl-editors-suggestion/";
@@ -190,7 +190,7 @@ ninja.data = [{
           title: "Sanderの送別会",
         
         description: "Sander van Haagenの送別会",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2024/sander-farewell/";
@@ -201,7 +201,7 @@ ninja.data = [{
           title: "OISTでFCPワークショップ",
         
         description: "OISTでの量子技術ワークショップに参加",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2024/fcp-workshop-oist/";
@@ -212,7 +212,7 @@ ninja.data = [{
           title: "RIKENの桜が満開",
         
         description: "RIKEN和光キャンパスの春の桜",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2024/cherry-blossoms-2024/";
@@ -223,7 +223,7 @@ ninja.data = [{
           title: "研究成果がResearch Newsに掲載！",
         
         description: "研究論文がRIKEN Research Newsで紹介",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2024/research-news/";
@@ -234,7 +234,7 @@ ninja.data = [{
           title: "Xianjingの送別会",
         
         description: "Xianjing Zhouの送別会",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2023/xianjing-farewell/";
@@ -245,7 +245,7 @@ ninja.data = [{
           title: "IvanがULT2022でベストポスター賞受賞！",
         
         description: "ULT2022でベストポスター賞受賞",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2022/ivan-best-poster-ult2022/";
@@ -256,7 +256,7 @@ ninja.data = [{
           title: "RIKENの桜が満開",
         
         description: "RIKEN和光キャンパスの春の桜",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2022/cherry-blossoms-2022/";
@@ -267,7 +267,7 @@ ninja.data = [{
           title: "nextrendasiaに記事が掲載！",
         
         description: "nextrendasiaに研究紹介記事が掲載",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2021/nextrendasia-article/";
@@ -278,7 +278,7 @@ ninja.data = [{
           title: "やっぱり物理が好き",
         
         description: "第6回「やっぱり物理が好き！」で講演",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2021/yappari-butsuri/";
@@ -289,7 +289,7 @@ ninja.data = [{
           title: "量子ICTインタビュー",
         
         description: "量子ICTフォーラムのインタビュー",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2021/quantum-ict-interview/";
@@ -300,7 +300,7 @@ ninja.data = [{
           title: "QFS2021",
         
         description: "QFS2021国際会議",
-        section: "",
+        section: "Posts",
         handler: () => {
           
             window.location.href = "/lab-hp/ja/blog/2021/qfs2021/";
@@ -314,115 +314,115 @@ ninja.data = [{
             },},{id: "news-トンネルダイオード発振器を用いた極低温マイクロ波源を開発-商用源より優れた振幅安定性を実現しました",
           title: 'トンネルダイオード発振器を用いた極低温マイクロ波源を開発。商用源より優れた振幅安定性を実現しました。',
           description: "",
-          section: "",},{id: "news-ポスドク-インターン-3ヶ月以上-を募集中です-erika-kawakami-riken-jpまでご連絡ください",
+          section: "News",},{id: "news-ポスドク-インターン-3ヶ月以上-を募集中です-erika-kawakami-riken-jpまでご連絡ください",
           title: 'ポスドク・インターン（3ヶ月以上） を募集中です。erika.kawakami@riken.jpまでご連絡ください。',
           description: "",
-          section: "",},{id: "news-プラズモン論文がarxivに公開されました-液体ヘリウム表面上の浮揚電子の集団励起-プラズモンモード-に関する新しい研究成果です",
+          section: "News",},{id: "news-プラズモン論文がarxivに公開されました-液体ヘリウム表面上の浮揚電子の集団励起-プラズモンモード-に関する新しい研究成果です",
           title: 'プラズモン論文がarXivに公開されました！液体ヘリウム表面上の浮揚電子の集団励起（プラズモンモード）に関する新しい研究成果です。',
           description: "",
-          section: "",},{id: "projects-project-7",
+          section: "News",},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
-          section: "",handler: () => {
+          section: "Projects",handler: () => {
               window.location.href = "/lab-hp/ja/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
-          section: "",handler: () => {
+          section: "Projects",handler: () => {
               window.location.href = "/lab-hp/ja/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
-          section: "",handler: () => {
+          section: "Projects",handler: () => {
               window.location.href = "/lab-hp/ja/projects/9_project/";
             },},{id: "projects-液体ヘリウム上の電子",
           title: '液体ヘリウム上の電子',
           description: "清浄な環境でのスケーラブルな電子スピン量子ビット",
-          section: "",handler: () => {
+          section: "Projects",handler: () => {
               window.location.href = "/lab-hp/ja/projects/ja/1_project/";
             },},{id: "projects-固体ネオン上の電子",
           title: '固体ネオン上の電子',
           description: "単一電子の電荷・スピン量子ビットの実現",
-          section: "",handler: () => {
+          section: "Projects",handler: () => {
               window.location.href = "/lab-hp/ja/projects/ja/2_project/";
             },},{id: "projects-極低温エレクトロニクス",
           title: '極低温エレクトロニクス',
           description: "量子ビット数の拡張に向けた極低温マイクロ波源の開発",
-          section: "",handler: () => {
+          section: "Projects",handler: () => {
               window.location.href = "/lab-hp/ja/projects/ja/3_project/";
             },},{id: "projects-projeto-4",
           title: 'projeto 4',
           description: "outro sem imagem",
-          section: "",handler: () => {
+          section: "Projects",handler: () => {
               window.location.href = "/lab-hp/ja/projects/ja/4_project/";
             },},{id: "projects-projeto-5",
           title: 'projeto 5',
           description: "um projeto com imagem de fundo",
-          section: "",handler: () => {
+          section: "Projects",handler: () => {
               window.location.href = "/lab-hp/ja/projects/ja/5_project/";
             },},{id: "projects-projeto-6",
           title: 'projeto 6',
           description: "um projeto sem imagem",
-          section: "",handler: () => {
+          section: "Projects",handler: () => {
               window.location.href = "/lab-hp/ja/projects/ja/6_project/";
             },},{
         id: 'social-email',
-        title: '',
-        section: '',
+        title: 'Send an email',
+        section: 'Socials',
         handler: () => {
           window.open("mailto:%72%69%6B%65%6E%45%6F%6E%48%65@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
         title: 'Inspire HEP',
-        section: '',
+        section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
         id: 'social-rss',
         title: 'RSS Feed',
-        section: '',
+        section: 'Socials',
         handler: () => {
           window.open("/lab-hp/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
-        section: '',
+        section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
           id: 'lang-en-us',
           title: 'en-us',
-          section: '',
+          section: 'Languages',
           handler: () => {
             window.location.href = "/lab-hp" + updatedUrl;
           },
         },{
       id: 'light-theme',
-      title: '',
-      description: '',
-      section: '',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
       handler: () => {
         setThemeSetting("light");
       },
     },
     {
       id: 'dark-theme',
-      title: '',
-      description: '',
-      section: '',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
       handler: () => {
         setThemeSetting("dark");
       },
     },
     {
       id: 'system-theme',
-      title: '',
-      description: '',
-      section: '',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
       handler: () => {
         setThemeSetting("system");
       },
