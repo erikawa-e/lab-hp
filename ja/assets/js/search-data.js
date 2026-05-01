df903a9bb2587cfc8fb3d9b77d0fc779a40f5f2d
erikawa-e <79493589+erikawa-e@users.noreply.github.com>
2026-05-01 23:37:02 +0000

    Deploying to gh-pages from @ erikawa-e/lab-hp@a338a5100b583c27b5bc0073a70c36567e3bd85a 🚀

--- ja/assets/js/search-data.js
+++ ja/assets/js/search-data.js
@@ -71,7 +71,7 @@ ninja.data = [{
           },
         },{id: "nav-グラント",
           title: "グラント",
-          description: "本研究は以下の機関からのご支援を受けています。深く感謝申し上げます。",
+          description: "以下の助成・支援を受けて研究を行っています。深く感謝申し上げます。",
           section: "Navigation menu",
           handler: () => {
             window.location.href = "/lab-hp/ja/grants/";
